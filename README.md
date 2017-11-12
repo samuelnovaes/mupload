c0029fe0894fc4242ea7a0d0a71d332fce3898a7
Samuel Novaes <samuel.novaes.96@gmail.com>
2017-11-12 17:35:45 -0200

    primeiro envio

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # upload-ajax
 Upload com Ajax e back-end em Node.js
+
+```bash
+git clone https://github.com/samuelnovaes/upload-ajax.git
+cd upload-ajax
+npm install
+npm start
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+let express = require('express')
+let path = require('path')
+let multipart = require('connect-multiparty')
+let fs = require('fs')
+let app = express()
+
+app.use(express.static(path.join(__dirname, 'static')))
+app.use(multipart())
+
+app.post('/upload', (req, res) => {
+	let arquivo = req.files.arquivo
+	fs.copyFile(arquivo.path, path.join(__dirname, 'uploads', path.basename(arquivo.path)), err => {
+		if(err) throw err
+		fs.unlink(arquivo.path, err => {
+			if(err) throw err
+			delete req.files.arquivo
+			res.send('Arquivo enviado!')
+		})
+	})
+})
+
+app.listen(8080, ()=>{
+	console.log('ok')
+})
