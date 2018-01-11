ea8dbfe6ab4afa4b153bac275790f4b879bb63a8
Eduardo <alvesil.eduardo.silva@gmail.com>
2018-01-11 02:13:12 -0200

    Funcionando no Windows

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let path = require('path')
 let multipart = require('connect-multiparty')
-let fs = require('fs')
+let fs = require('fs-extra')
 let app = express()
 
 app.use(express.static(path.join(__dirname, 'static')))
@@ -9,7 +9,7 @@ app.use(multipart())
 
 app.post('/upload', (req, res) => {
 	let arquivo = req.files.arquivo
-	fs.copyFile(arquivo.path, path.join(__dirname, 'uploads', path.basename(arquivo.path)), err => {
+	fs.copy(arquivo.path, path.join(__dirname, 'uploads', arquivo.originalFilename), err => {
 		if(err) throw err
 		fs.unlink(arquivo.path, err => {
 			if(err) throw err
@@ -19,6 +19,6 @@ app.post('/upload', (req, res) => {
 	})
 })
 
-app.listen(8080, ()=>{
-	console.log('Abra a URL http://localhost:8000 no seu navegador!')
+app.listen(80, ()=>{
+	console.log('Abra a URL http://localhost:80 no seu navegador!')
 })
