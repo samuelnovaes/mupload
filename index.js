352f295a94b61c4c5c6c159b001414ab7dbe95d6
Samuel Novaes <samuel.novaes.96@gmail.com>
2018-01-15 13:55:54 -0200

    File download and auth

--- index.js
+++ index.js
@@ -3,36 +3,91 @@ let formidable = require('formidable')
 let path = require('path')
 let fs = require('fs-extra')
 let ip = require('ip')
+let session = require('express-session')
 let app = express()
+let password
 
-app.use(express.static(path.join(__dirname, 'static')))
+fs.readFile(path.join(__dirname, 'password.txt'), 'utf-8', (err, data) =>{
+	if(err) throw err
+	password = data.trim()
 
-app.post('/', (req, res) => {
-	let form = new formidable.IncomingForm()
-	form.multiples = true
-	form.uploadDir = path.join(__dirname, 'uploads')
-	form.parse(req, async (err, fields, files) => {
-		if(err) res.status(500).send(err.message)
-		let fileList = Array.isArray(files.files) ? files.files : [files.files]
-		for(let i in fileList){
-			let file = fileList[i]
-			if(file.name){
-				try {
-					await fs.rename(file.path, path.join(__dirname, 'uploads', file.name))
+	app.use(session({
+		secret: password,
+		resave: false,
+		saveUninitialized: true
+	}))
+
+	app.use(express.static(path.join(__dirname, 'static')))
+
+	app.get('/', auth, (req, res) => {
+		res.sendFile(path.join(__dirname, 'views', 'index.html'))
+	})
+
+	app.post('/upload', auth, (req, res) => {
+		let form = new formidable.IncomingForm()
+		form.multiples = true
+		form.uploadDir = path.join(__dirname, 'uploads')
+		form.parse(req, async (err, fields, files) => {
+			if(err) res.status(500).send(err.message)
+			let fileList = Array.isArray(files.files) ? files.files : [files.files]
+			for(let i in fileList){
+				let file = fileList[i]
+				if(file.name){
+					try {
+						await fs.rename(file.path, path.join(__dirname, 'uploads', file.name))
+					}
+					catch(err){
+						res.status(500).send(err.message)
+					}
 				}
-				catch(err){
-					res.status(500).send(err.message)
+				else {
+					fs.remove(file.path)
+					res.status(500).send('No file chosen')
 				}
 			}
-			else {
-				fs.remove(file.path)
-				res.status(500).send('No file chosen')
-			}
-		}
-		res.end()
+			res.end()
+		})
+	})
+
+	app.get('/files', auth, (req, res) => {
+		fs.readdir(path.join(__dirname, 'uploads'), (err,files) => {
+			if(err) res.status(500).send(err.message)
+			res.json(files)
+		})
+	})
+
+	app.get('/download/:file', auth, (req, res) => {
+		let file = path.join(__dirname, 'uploads', req.params.file)
+		fs.pathExists(file, (err, exists) => {
+			if(err) res.status(500).send(err.message)
+			if(exists) res.download(file)
+			else res.status(500).send('File does not exists')
+		})
+	})
+
+	app.post('/auth', (req, res) => {
+		let form = new formidable.IncomingForm()
+		form.parse(req, async (err, fields, files) => {
+			if(err) res.status(500).send(err.message)
+			req.session.password = fields.password
+			res.redirect('/')
+		})
+	})
+
+	fs.ensureDir(path.join(__dirname, 'uploads'), err => {
+		if(err) throw err
+		app.listen(8080, () => {
+			console.log(`http://${ip.address()}:8080`)
+		})
 	})
 })
 
-app.listen(8080, () => {
-	console.log(`http://${ip.address()}:8080`)
-})
+function auth(req, res, next){
+	let pass = req.session.password
+	if(pass == password){
+		next()
+	}
+	else {
+		res.sendFile(path.join(__dirname, 'views', 'auth.html'))
+	}
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,21 @@
 let form = document.getElementById('form')
 let status = document.getElementById('status')
 let btn = document.getElementById('btn')
+let fileList = document.getElementById('file-list')
+
 form.onsubmit = e => {
 	btn.disabled = true
 	let data = new FormData(form)
-	axios.post('/', data, {
+	axios.post('/upload', data, {
 		onUploadProgress: e => {
 			let progress = parseInt(((e.loaded / e.total) * 100))
-			status.innerText = `Uploaded ${progress}%`
+			status.innerText = `Uploading ${progress}%`
 		}
 	})
 	.then(response => {
 		status.innerText = `Uploaded`
 		btn.disabled = false
+		reload()
 	})
 	.catch(err => {
 		status.innerText = `Error: ${err.response.data}`
@@ -20,3 +23,18 @@ form.onsubmit = e => {
 	})
 	e.preventDefault()
 }
+
+function reload(){
+	fileList.innerHTML = ''
+	axios.get('/files')
+	.then(response => {
+		response.data.forEach(item => {
+			fileList.innerHTML += `<li><a href="/download/${item}">${item}</a></li>`
+		})
+	})
+	.catch(err => {
+		status.innerText = `Error: ${err.response.data}`
+	})
+}
+
+reload()
